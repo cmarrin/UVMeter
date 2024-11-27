093b411d26de09d894e74eb72d61d1ceede06eb6
Chris Marrin <chris@marrin.com>
2024-11-27 07:54:26 -0800

    Added initial files

---
+++ mac/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  UVMeter
+//
+//  Created by Chris Marrin on 11/27/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
